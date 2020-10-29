65c626c63c1507f21b90742e3ecdc7ae79f86b57
JorisKunk <joris.kunkeler@student.hu.nl>
2020-10-29 13:58:30 +0100

    update ir_receiver

--- ir-receiver/main.cpp
+++ ir-receiver/main.cpp
@@ -1,83 +1,76 @@
-/*#include "hwlib.hpp"
-#include <bitset>
+#include "hwlib.hpp"
+#include "rtos.hpp"
 
-int main( void ) {
-	auto tsop_signal = hwlib::target::pin_in(hwlib::target::pins::d8);
-	hwlib::wait_us(1);
-	uint16_t message = 0x00;
+class ir_receiver : public rtos::task<>{
+private:
 
-	while(true){
-		tsop_signal.refresh();
-		if(!tsop_signal.read() == 1) {
+	enum state_t{
+		idle,
+		receive_bits
+	};
+
+	state_t state = idle;
+
+	hwlib::target::pin_in& signal;
+
+	rtos::timer receive_timer = {this, "receive_timer"};
+	long long int delay = 1200 * rtos::us;
+	rtos::timer receive_timer_1us = {this, "receive_timer_1us"};
+	long long int start_delay_1us = 1 * rtos::us;
+
+    message_translation& messageTranslation;
+
+    uint16_t message = 0x00;
+
+	void main(){
+		while(true){
+			switch(state){
+				case idle:{
+					//entry
+					signal.refresh();
+					//transition
+					if (signal.read() == 1){
+						state = receive_bits;
+					}
+					receive_timer.set(delay);
+					wait(receive_timer);
+					break;
+				}
+				case receive_bits:{
+					//entry
+					ir_receive(message);
+					//transition
+					state = idle;
+					break;
+				}
+			}
+		}
+	}
+
+	void ir_receive(uint16_t& message){
+		signal.refresh();
+		if(signal.read() == 1) {
 			for (int i = 0; i < 16; ++i) {
-				hwlib::wait_us(1200);
-				tsop_signal.refresh();
-				if (!tsop_signal.read() == 1) {
+				receive_timer.set(delay);
+				wait(receive_timer);
+				signal.refresh();
+				if (signal.read() == 1) {
 					message = message << 1;
 					message |= 0x01;
 				} else{
 					message = message << 1;
 				}
-				hwlib::wait_us(1200);
+				receive_timer.set(delay);
+				wait(receive_timer);
 			}
-			hwlib::cout << "message: "<<message<<'\n';
-			break;
+			messageTranslation.write(message);
+			message = 0x00;
 		}
-	}
-}*/
-
-#include "hwlib.hpp"
-#include "rtos.hpp"
-
-class ir_receive : public rtos::task<>{
-private:
-	enum state_t{
-		Idle,
-		Receive_bits
-	};
-	hwlib::target::pin_in signal;
-	rtos::timer send_timer_long = {this, "send_timer_long"};
-	long long int delay_long = 1600 * rtos::us;
-	rtos::timer send_timer_short = {this, "send_timer_short"};
-	long long int delay_short = 800 * rtos::us;
-	rtos::timer send_timer_1us = {this, "send_timer_1us"};
-	long long int start_delay_1us = 1 * rtos::us;
-
-	messagetranslation& messagetranslation;
-
-	void receive_bits(){
-		while(true){
-			tsop_signal.refresh();
-			if(!tsop_signal.read() == 1) {
-				for (int i = 0; i < 16; ++i) {
-					hwlib::wait_us(1200);
-					tsop_signal.refresh();
-					if (!tsop_signal.read() == 1) {
-						message = message << 1;
-						message |= 0x01;
-					} else{
-						message = message << 1;
-					}
-					hwlib::wait_us(1200);
-				}
-				//hwlib::cout << "message: "<<message<<'\n';
-				return message;
-				break;
-			}
-		}
-	}
-
-	void main(){
-		receive_bits();
 	}
 
 public:
-	ir_receive(hwlib::target::pin_in& signal_):
-	signal(signal_)
+	ir_receiver(hwlib::target::pin_in& signal_, message_translation& messageTranslation_):
+	    signal(signal_),
+        messageTranslation(messageTranslation_)
 	{}
-
-	void add(uint16_t & message){
-		received_bits_channel.write(message);
-	}
 };
-
